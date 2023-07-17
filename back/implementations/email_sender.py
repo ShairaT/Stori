1209b9161b030439bc723e356e37f59a158abef1
Ayelen Luciano <ayelenluciano@MacBook-Pro-de-Ayelen.local>
2023-07-17 13:11:04 -0300

    updated readme

--- back/implementations/email_sender.py
+++ back/implementations/email_sender.py
@@ -15,34 +15,36 @@ class EmailSender:
         self.sg = SendGridAPIClient(os.getenv('SENDGRID_API_KEY'))
 
     def send_email(self, recipient_email, subject, body, article_file_path, newsletter_id):
+        try:
+            encrypted_recipient = encrypt_email(recipient_email)
+            unsubscribe_url = 'http://localhost:9999/newsletters/' + \
+                newsletter_id + '/' + encrypted_recipient + '/unsubscribe'
+            
+            mail = Mail(
+                from_email='ayelenmaite2000@gmail.com',
+                to_emails=recipient_email,
+                subject=subject,
+                plain_text_content=body,
+                html_content=get_email_html_template(unsubscribe_url)
+                )
 
-        encrypted_recipient = encrypt_email(recipient_email)
-        unsubscribe_url = 'http://localhost:8080/newsletters/' + \
-            newsletter_id + '/' + encrypted_recipient + '/unsubscribe'
-        
-        mail = Mail(
-            from_email='ayelenmaite2000@gmail.com',
-            to_emails=recipient_email,
-            subject=subject,
-            plain_text_content=body,
-            html_content=get_email_html_template(unsubscribe_url)
-            )
+            file_type = get_file_type(article_file_path)
+            image_content_base64 = convert_url_firebase_to_base64(
+                article_file_path)
 
-        file_type = get_file_type(article_file_path)
-        image_content_base64 = convert_url_firebase_to_base64(
-            article_file_path)
+            attachment = Attachment()
+            attachment.file_content = image_content_base64
+            attachment.file_name = FileName('article')
+            attachment.file_type = FileType(file_type)
+            attachment.content_id = "image_cid"
+            attachment.disposition = Disposition('attachment')
+            mail.attachment = attachment
 
-        attachment = Attachment()
-        attachment.file_content = image_content_base64
-        attachment.file_name = FileName('article')
-        attachment.file_type = FileType(file_type)
-        attachment.content_id = "image_cid"
-        attachment.disposition = Disposition('attachment')
-        mail.attachment = attachment
+            response = self.sg.send(mail)
 
-        response = self.sg.send(mail)
-
-        if response.status_code == 202:
-            return True
-        else:
-            return False
+            if response.status_code == 202:
+                return True
+            else:
+                return False
+        except Exception as e:
+            raise Exception(str(e))
